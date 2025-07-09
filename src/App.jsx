3254a6f761fb96123fe26ceb0e8ff82ec2f924cd
NandiniBure <nandinibure@gmail.com>
2025-07-09 22:51:29 +0530

    react-tailwind-template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl text-blue-700">
+      Hello from React + Tailwind CSS!
+    </div>
+  )
+}
+
+export default App
